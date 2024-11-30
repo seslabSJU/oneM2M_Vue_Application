684255afc27ca63bca1d94997916aa4194137ac5
poosic <fkfkvktm310@naver.com>
2024-11-30 11:43:37 +0900

    Create DeleteResource.vue
    DeleteRS Code

---
+++ vue-project/src/components/DeleteResource.vue
@@ -0,0 +1,126 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      res_name: '',
+      data_obj: {
+        Platform_addr: '127.0.0.1:3000',
+        Res_Id: 'TinyIoT',
+        X_M2M_RI: '12345',
+        X_M2M_RVI: '2a',
+        X_M2M_Origin: 'CAdmin',
+        Accept: 'application/json',
+
+        lbl: '',
+        mni: '',
+        mbs: '',
+        ty: '',
+        op: '',
+        api: '',
+        rr: true,
+        con: '',
+        rn: '',
+        nu: '',
+        enc: '',
+        exc: '10',
+        net: [3, 4],
+        poa: [],
+        cb: '',
+        csi: '',
+        srv: ['1', '2', '2a'],
+        pv_acr: [],
+        pvs_acr: [],
+        mid: [],
+        mnm: '',
+        mt: '',
+        csy: '',
+      },
+      req_fields: [
+        { key: 'X-M2M-RI', class: 'text-center' },
+        { key: 'X-M2M-Origin', class: 'text-center' },
+        { key: 'X-M2M-RVI', class: 'text-center' },
+        { key: 'Content-Type', class: 'text-center' },
+        { key: 'Accept', class: 'text-center' },
+      ],
+      req_items: [
+        { 'X-M2M-RI': '', 'X-M2M-Origin': '', 'X-M2M-RVI': '', 'Content-Type': '', Accept: '' },
+      ],
+      res_fields: [
+        { key: 'X-M2M-RI', class: 'text-center' },
+        { key: 'X-M2M-RSC', class: 'text-center' },
+        { key: 'X-M2M-RVI', class: 'text-center' },
+        { key: 'Content-Length', class: 'text-center' },
+        { key: 'Content-Type', class: 'text-center' },
+      ],
+      res_items: [
+        {
+          'X-M2M-RI': '',
+          'X-M2M-RSC': '',
+          'X-M2M-RVI': '',
+          'Content-Length': '',
+          'Content-Type': '',
+        },
+      ],
+      headers_text: '',
+      query_Params: '',
+      request_text: '',
+      response_text: '',
+      res_mess: '',
+      res_errmess: '',
+      res_status: '',
+    }
+  },
+  methods: {
+    deleteRS() {
+      let url = "http://" + this.data_obj.Platform_addr + "/" + this.data_obj.Res_Id + this.query_Params;
+      const headers = {};
+      headers["X-M2M-RI"] = this.data_obj.X_M2M_RI;
+      headers["X-M2M-Origin"] = this.data_obj.X_M2M_Origin;
+      headers["X-M2M-RVI"] = "2a";
+      headers["Accept"] = this.data_obj.Accept;
+
+      axios
+        .delete(url, { headers })
+        .then((response) => {
+          this.res_mess = response.data;
+          this.res_status = response.status;
+
+          let headers = {};
+          headers["X-M2M-RI"] = response.headers["x-m2m-ri"];
+          headers["X-M2M-RSC"] = response.headers["x-m2m-rsc"];
+          headers["X-M2M-RVI"] = response.headers["x-m2m-rvi"];
+          headers["Content-Length"] = response.headers["content-length"];
+          headers["Content-Type"] = response.headers["content-type"];
+          this.response_header_change(headers);
+
+          return (this.response_text = JSON.stringify(
+            this.res_mess,
+            undefined,
+            2
+          ));
+        })
+        .catch((error) => {
+          this.res_errmess = error.response.data;
+
+          if (error.response.status === 404) {
+            this.res_status = error.response.status;
+          } else if (error.response.status === 403) {
+            this.res_status = error.response.status;
+          }
+          F
+          let headers = {};
+          headers["X-M2M-RI"] = error.response.headers["x-m2m-ri"];
+          headers["X-M2M-RSC"] = error.response.headers["x-m2m-rsc"];
+          headers["X-M2M-RVI"] = error.response.headers["x-m2m-rvi"];
+          headers["Content-Length"] = error.response.headers["content-length"];
+          headers["Content-Type"] = error.response.headers["content-type"];
+          this.response_header_change(headers);
+
+          return (this.response_text = this.res_errmess);
+        });
+    },
+  },
+}
+</script>
