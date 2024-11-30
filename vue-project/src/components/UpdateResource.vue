a3ac46ff1f54a6671879ef1211716b5f0c6d75c5
Nam Kyoung Hwan <nkh0721@gamil.com>
2024-11-30 11:44:30 +0900

    update resource added

--- vue-project/src/components/UpdateResource.vue
+++ vue-project/src/components/UpdateResource.vue
@@ -37,6 +37,13 @@ export default {
         mt: '',
         csy: '',
       },
+      update_fields: {
+      ae: ['lbl'],
+      cnt: ['lbl'], //cin은 업데이트 가능한 attribute 존재X 
+      sub: ['nu'],
+      },
+      current_resource_type: '', // 현재 업데이트하려는 리소스 타입
+      is_updating: false, // 업데이트 모드 여부
       req_fields: [
         { key: 'X-M2M-RI', class: 'text-center' },
         { key: 'X-M2M-Origin', class: 'text-center' },
@@ -63,6 +70,7 @@ export default {
           'Content-Type': '',
         },
       ],
+      query_Params: '',
       headers_text: '',
       request_text: '',
       response_text: '',
@@ -89,6 +97,36 @@ export default {
       this.res_items[0]['Content-Length'] = obj['Content-Length']
       this.res_items[0]['Content-Type'] = obj['Content-Type']
       this.$refs.restable.refresh()
+    },
+    updateAE() {
+        let ae_obj = {
+        'm2m:ae': {
+            'lbl': ['update1'],
+          }
+        };
+        this.data_obj.Body = ae_obj;
+        this.data_obj['Content-Type'] = 'application/json;ty=2';
+        this.update_request();
+    },
+    updateContainer() {
+        let cnt_obj = {
+        'm2m:cnt': {
+            'lbl': ['update1'],
+          }
+        };
+        this.data_obj.Body = cnt_obj;
+        this.data_obj['Content-Type'] = 'application/json;ty=3';
+        this.update_request();
+    },
+    updateSubscriptionResource() {
+        let sub_obj = {
+        'm2m:sub': {
+            'nu': [''],
+          }
+        };
+        this.data_obj.Body = sub_obj;
+        this.data_obj['Content-Type'] = 'application/json;ty=23';
+        this.update_request();
     },
     createAE() {
       let ae_obj = {}
@@ -259,6 +297,73 @@ export default {
       this.request_header_change(headers)
       return (this.request_text = JSON.stringify(this.req_display_obj, undefined, 2))
     },
+    update_request() {
+        let url = "http://" + this.data_obj.Platform_addr + "/" + this.data_obj.Res_Id + '/' + this.data.obj.query_Params;
+        const headers = {
+            "X-M2M-RI": this.data_obj.X_M2M_RI,
+            "X-M2M-Origin": this.data_obj.X_M2M_Origin,
+            "X-M2M-RVI": "2a",
+            "Content-Type": this.data_obj["Content-Type"],
+            "Accept": this.data_obj.Accept
+        };
+
+        let body = this.data_obj.Body;
+
+        // PUT 요청 먼저 시도
+        axios.put(url, body, { headers })
+        .then(response => {
+          this.handleSuccess(response);
+        })
+        .catch(error => {
+        // 리소스가 없는 경우 (404) POST 요청으로 생성 시도
+          if (error.response && error.response.status === 404) {
+            console.log("Resource not found, trying to create with POST");
+            axios.post(url, body, { headers })
+            .then(response => {
+              this.handleSuccess(response);
+            })
+            .catch(error => {
+              this.handleError(error);
+            });
+          } else {
+            this.handleError(error);
+          }
+      });
+    },
+    // 성공 응답 처리를 위한 헬퍼 메소드
+    handleSuccess(response) {
+      this.res_mess = response.data;
+      this.res_status = response.status;
+    
+      const headers = {
+        "X-M2M-RI": response.headers["x-m2m-ri"],
+        "X-M2M-RSC": response.headers["x-m2m-rsc"],
+        "X-M2M-RVI": response.headers["x-m2m-rvi"],
+        "Content-Length": response.headers["content-length"],
+        "Content-Type": response.headers["content-type"]
+      };
+    
+      this.response_header_change(headers);
+      this.response_text = JSON.stringify(this.res_mess, undefined, 2);
+    },
+
+  // 에러 응답 처리를 위한 헬퍼 메소드
+    handleError(error) {
+    this.res_errmess = error.response.data;
+    this.res_status = error.response.status;
+    
+      const headers = {
+        "X-M2M-RI": error.response.headers["x-m2m-ri"],
+        "X-M2M-RSC": error.response.headers["x-m2m-rsc"],
+        "X-M2M-RVI": error.response.headers["x-m2m-rvi"],
+        "Content-Length": error.response.headers["content-length"],
+        "Content-Type": error.response.headers["content-type"]
+      };
+    
+      this.response_header_change(headers);
+      this.response_text = this.res_errmess;
+    },
+    // 리소스 생성 요청
     post_request() {
       let url =
         "http://" + this.data_obj.Platform_addr + "/" + this.data_obj.Res_Id;
@@ -307,6 +412,6 @@ export default {
           return (this.response_text = this.res_errmess);
         });
     },
-  },
+  }
 }
 </script>
